aa571416727e061f73bdffe2e690084e8ec0721f
Jose Tong <jose.e.tong@gmail.com>
2018-11-06 10:21:04 +0000

    add start/stop scripts

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,8 +3,8 @@
 set -e
 
 pushd /var/www/html/portfolio/budget2
-/usr/bin/node /usr/local/lib/node_modules/forever/bin/forever start app.js
+/usr/local/bin/forever start app.js
 popd
 pushd /var/www/html/portfolio/chest_tracker/
-/usr/bin/node /usr/local/lib/node_modules/forever/bin/forever start index.js
+/usr/local/bin/forever start index.js
 popd
--- deploy/stop.sh
+++ deploy/stop.sh
@@ -3,8 +3,8 @@
 set -e
 
 pushd /var/www/html/portfolio/budget2
-/usr/bin/node /usr/local/lib/node_modules/forever/bin/forever stop app.js
+/usr/local/bin/forever stop app.js
 popd
 pushd /var/www/html/portfolio/chest_tracker/
-/usr/bin/node /usr/local/lib/node_modules/forever/bin/forever stop index.js
+/usr/local/bin/forever stop index.js
 popd
